8c70d5133afb061cac8974e2872e18d70f815df0
impoimpoimpo <aimpo@outlook.com>
2020-05-24 22:14:08 +0930

    Added start and credit scenes

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
